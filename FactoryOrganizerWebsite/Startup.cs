442cfc1948699bbe977ea2be4d4d877fcda1d5e9
Alex Waychoff <AlexWaychoff@gmail.com>
2018-01-19 13:24:25 -0600

    first commit

---
+++ FactoryOrganizerWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FactoryOrganizerWebsite.Startup))]
+namespace FactoryOrganizerWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
