4a894b0afeb1a73ab4ed2b98596377c1f02d4942
Alex Waychoff <AlexWaychoff@gmail.com>
2018-01-22 15:43:58 -0600

    added database for FactoryOrganizer

---
+++ FactoryOrganizerWebsite/Models/EmployeeScrap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FactoryOrganizerWebsite.Models
+{
+    public class EmployeeScrap
+    {
+        public int EmployeeId { get; set; }
+        public DateTime DayOfProduction { get; set; }
+        public int TotalScrap { get; set; }
+        //public int ScrapFromEXTRA { get; set; }
+        public int TotalPieces { get; set; }
+        public float AverageCycleTime { get; set; }
+    }
+}
