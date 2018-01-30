7b8f59cf0727b82c672e12236faaef66d43dee41
Alex Waychoff <AlexWaychoff@gmail.com>
2018-01-30 08:51:58 -0600

    added model so programs can send file path

---
+++ FactoryOrganizerWebsite/Models/FilePathToPrograms.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FactoryOrganizerWebsite.Models
+{
+    public class FilePathToPrograms
+    {
+        public int FilePathToProgramsID { get; set; }
+        public string ProgramType { get; set; }
+        public int FilePath { get; set; }
+    }
+}
